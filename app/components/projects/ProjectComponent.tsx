ba2ab25e8c72d6fec64168402f1252dcfb9abc62
Kiran Gadhavi <kirangadhavi@hotmail.com>
2024-10-25 01:46:47 +0100

    add one more project with show more and show less projects button

--- app/components/projects/ProjectComponent.tsx
+++ app/components/projects/ProjectComponent.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react"; // Import useState to manage state
 import Image from "next/image";
 import { motion } from "framer-motion";
 import Link from "next/link";
+
+// Projects data array
 
 const projects = [
   {
@@ -226,14 +228,71 @@ const projects = [
       },
     ],
   },
+  {
+    id: 7,
+    title: "trickr_street",
+    description:
+      "Trickr Street is a Next.js application that features an interactive Google Map with custom markers and a night mode theme. It's designed to provide a visually appealing and user-friendly map experience.",
+    href: "https://trickrstreet.vercel.app",
+    githubLink: "https://github.com/KiranGadhavi/trickr_street",
+    projectImage: "/trick.png",
+    technologies: [
+      {
+        src: "https://img.shields.io/badge/-Next.js-000000?style=flat-square&logo=next.js&logoColor=white",
+        alt: "Next.js",
+      },
+      {
+        src: "https://img.shields.io/badge/-React-61DAFB?style=flat-square&logo=react&logoColor=black",
+        alt: "React",
+      },
+      {
+        src: "https://img.shields.io/badge/-JavaScript-F7DF1E?style=flat-square&logo=javascript&logoColor=black",
+        alt: "JavaScript",
+      },
+      {
+        src: "https://img.shields.io/badge/-Git-F05032?style=flat-square&logo=git&logoColor=white",
+        alt: "Git",
+      },
+      {
+        src: "https://img.shields.io/badge/-Supabase-3ECF8E?style=flat-square&logo=supabase&logoColor=white",
+        alt: "Supabase",
+      },
+      {
+        src: "https://img.shields.io/badge/-Tailwind_CSS-38B2AC?style=flat-square&logo=tailwind-css&logoColor=white",
+        alt: "Tailwind CSS",
+      },
+      {
+        src: "https://img.shields.io/badge/-Github-181717?style=flat-square&logo=github&logoColor=white",
+        alt: "Github",
+      },
+      {
+        src: "https://img.shields.io/badge/-Jest-C21325?style=flat-square&logo=jest&logoColor=white",
+        alt: "Jest",
+      },
+      {
+        src: "https://img.shields.io/badge/-Vercel-000000?style=flat-square&logo=vercel&logoColor=white",
+        alt: "Vercel",
+      },
+    ],
+  },
 ];
+
 const cardVariants = {
   hover: { scale: 1.05, boxShadow: "0px 4px 10px rgba(0,0,0,0.2)" },
   tap: { scale: 0.95 },
 };
 
-// export default ProjectComponent;
 const ProjectComponent = ({ title }: { title: string }) => {
+  // Step 1: Initialize state to manage the number of projects to display
+  const [visibleProjects, setVisibleProjects] = useState(4); // Show 4 projects initially
+
+  // Step 2: Create a function to toggle the number of projects shown
+  const toggleProjects = () => {
+    setVisibleProjects((prev) =>
+      prev === projects.length ? 4 : projects.length
+    ); // Show all or reset to 4
+  };
+
   return (
     <div className="container mx-auto py-16">
       {/* Section Title */}
@@ -259,93 +318,110 @@ const ProjectComponent = ({ title }: { title: string }) => {
 
       {/* Projects Grid */}
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
-        {projects.map((project) => (
-          <motion.div
-            key={project.id}
-            variants={cardVariants}
-            whileHover="hover"
-            whileTap="tap"
-            initial={{ opacity: 0, scale: 0.9 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ delay: 0.2, duration: 0.5 }}
-            className="font-white border border-gray-200 bg-green-50 bg-opacity-10 backdrop-blur-lg rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300"
-          >
-            {/* Project Image */}
-            {project.projectImage && (
-              <motion.div whileHover={{ scale: 1.1 }}>
-                <Image
-                  src={project.projectImage}
-                  alt={project.title}
-                  width={600}
-                  height={400}
-                  className="w-full h-48 object-fit transition-transform duration-300 transform hover:scale-110"
-                />
-              </motion.div>
-            )}
-            <div className="p-6">
-              {/* Project Title */}
-              <h3 className="text-2xl text-gray-400 font-bold mb-3 transition-colors duration-300 hover:text-blue-500">
-                {project.title}
-              </h3>
-
-              {/* Project Description */}
-              <p className="text-gray-600 mb-4">{project.description}</p>
-
-              {/* Technologies Used */}
-              <div className="flex space-x-2 mb-4">
-                {project.technologies.map((tech, index) => (
+        {projects.slice(0, visibleProjects).map(
+          (
+            project // Step 3: Use slice to control visible projects
+          ) => (
+            <motion.div
+              key={project.id}
+              variants={cardVariants}
+              whileHover="hover"
+              whileTap="tap"
+              initial={{ opacity: 0, scale: 0.9 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
+              className="font-white border border-gray-200 bg-green-50 bg-opacity-10 backdrop-blur-lg rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300"
+            >
+              {/* Project Image */}
+              {project.projectImage && (
+                <motion.div whileHover={{ scale: 1.1 }}>
                   <Image
-                    key={index}
-                    src={tech.src}
-                    alt={tech.alt}
-                    width={30}
-                    height={30}
-                    className="inline-block"
+                    src={project.projectImage}
+                    alt={project.title}
+                    width={600}
+                    height={400}
+                    className="w-full h-48 object-fit transition-transform duration-300 transform hover:scale-110"
                   />
-                ))}
-              </div>
+                </motion.div>
+              )}
+              <div className="p-6">
+                {/* Project Title */}
+                <h3 className="text-2xl text-gray-400 font-bold mb-3 transition-colors duration-300 hover:text-blue-500">
+                  {project.title}
+                </h3>
 
-              {/* Project Links */}
-              <div className="flex justify-between items-center">
-                {project.href && (
-                  <Link href={project.href}>
-                    <p className="text-gray-400 font-medium hover:underline hover:text-blue-500 flex items-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                        width="24"
-                        height="24"
-                        className="mr-2"
-                      >
-                        <path d="M14 3h7v7h-2V6.414l-9.293 9.293-1.414-1.414L17.586 5H14V3zM5 5h6V3H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-6h-2v6H5V5z" />
-                      </svg>
-                      Visit Project &rarr;
-                    </p>
-                  </Link>
-                )}
+                {/* Project Description */}
+                <p className="text-gray-600 mb-4">{project.description}</p>
 
-                {project.githubLink && (
-                  <Link href={project.githubLink}>
-                    <p className="text-gray-400 font-medium hover:underline hover:text-blue-500 flex items-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        fill="currentColor"
-                        className="mr-2"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12 .296c-6.624 0-12 5.376-12 12 0 5.304 3.438 9.8 8.205 11.385.6.111.82-.261.82-.581v-2.188c-3.338.724-4.04-1.616-4.04-1.616-.547-1.385-1.336-1.754-1.336-1.754-1.091-.745.082-.729.082-.729 1.205.083 1.839 1.238 1.839 1.238 1.072 1.834 2.809 1.304 3.494.997.109-.775.42-1.305.763-1.605-2.664-.303-5.466-1.332-5.466-5.931 0-1.311.469-2.383 1.238-3.225-.123-.303-.537-1.521.118-3.17 0 0 1.009-.324 3.303 1.23.957-.267 1.984-.4 3.004-.405 1.02.005 2.047.138 3.006.405 2.292-1.554 3.299-1.23 3.299-1.23.657 1.649.243 2.867.119 3.17.772.842 1.238 1.914 1.238 3.225 0 4.609-2.807 5.625-5.479 5.921.43.371.823 1.102.823 2.222v3.293c0 .323.219.694.827.576 4.767-1.589 8.201-6.084 8.201-11.385 0-6.624-5.376-12-12-12z" />
-                      </svg>
-                      View Code
-                    </p>
-                  </Link>
-                )}
+                {/* Technologies Used */}
+                <div className="flex space-x-2 mb-4">
+                  {project.technologies.map((tech, index) => (
+                    <Image
+                      key={index}
+                      src={tech.src}
+                      alt={tech.alt}
+                      width={30}
+                      height={30}
+                      className="inline-block"
+                    />
+                  ))}
+                </div>
+
+                {/* Project Links */}
+                <div className="flex justify-between items-center">
+                  {project.href && (
+                    <Link href={project.href}>
+                      <p className="text-gray-400 font-medium hover:underline hover:text-blue-500 flex items-center">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 24 24"
+                          width="24"
+                          height="24"
+                          className="mr-2"
+                        >
+                          <path d="M14 3h7v7h-2V6.414l-9.293 9.293-1.414-1.414L17.586 5H14V3zM5 5h6V3H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-6h-2v6H5V5z" />
+                        </svg>
+                        Visit Project &rarr;
+                      </p>
+                    </Link>
+                  )}
+
+                  {project.githubLink && (
+                    <Link href={project.githubLink}>
+                      <p className="text-gray-400 font-medium hover:underline hover:text-blue-500 flex items-center">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          fill="currentColor"
+                          className="mr-2"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M12 .296c-6.624 0-12 5.376-12 12 0 5.304 3.438 9.8 8.205 11.385.6.111.82-.261.82-.581v-2.188c-3.338.724-4.04-1.616-4.04-1.616-.547-1.385-1.336-1.754-1.336-1.754-1.091-.745.082-.729.082-.729 1.205.083 1.839 1.238 1.839 1.238 1.072 1.834 2.809 1.304 3.494.997.109-.775.42-1.305.763-1.605-2.664-.303-5.466-1.332-5.466-5.931 0-1.311.469-2.383 1.238-3.225-.123-.303-.537-1.521.118-3.17 0 0 1.009-.324 3.303 1.23.957-.267 1.984-.4 3.004-.405 1.02.005 2.047.138 3.006.405 2.292-1.554 3.299-1.23 3.299-1.23.657 1.649.243 2.867.119 3.17.772.842 1.238 1.914 1.238 3.225 0 4.609-2.807 5.625-5.479 5.921.43.371.823 1.102.823 2.222v3.293c0 .323.219.694.827.576 4.767-1.589 8.201-6.084 8.201-11.385 0-6.624-5.376-12-12-12z" />
+                        </svg>
+                        View Code
+                      </p>
+                    </Link>
+                  )}
+                </div>
               </div>
-            </div>
-          </motion.div>
-        ))}
+            </motion.div>
+          )
+        )}
+      </div>
+
+      {/* Step 4: Add Show More/Less button */}
+      <div className="flex justify-center mt-8">
+        <button
+          onClick={toggleProjects}
+          className="px-4 py-2 text-white bg-blue-500 hover:bg-blue-600 rounded transition duration-300"
+        >
+          {visibleProjects === projects.length
+            ? "Show Less Projects"
+            : "Show More Projects"}{" "}
+          {/* Dynamic button text */}
+        </button>
       </div>
     </div>
   );
